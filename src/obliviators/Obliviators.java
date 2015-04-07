72b304f49536a6a428e149b43e63d18abeed0e42
alkellogg <audreykellogg@Audreys-MacBook-Air.local>
2015-04-07 10:57:35 -0700

    initial check-in

---
+++ src/obliviators/Obliviators.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package obliviators;
+
+import environment.ApplicationStarter;
+
+
+/**
+ *
+ * @author audreykellogg
+ */
+public class Obliviators{
+  
+
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ApplicationStarter.run("Oblivion", new OblivionEnvironment());
+        // TODO code application logic here
+    }
+    
+}
